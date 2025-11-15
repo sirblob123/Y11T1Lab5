7191808a05302062fa1c2d9b18eb12b9bedabf03
Ian Yu <ian@Jialins-MacBook-Pro.local>
2025-11-15 14:05:32 +0800

    Y11T1Lab5

---
+++ src/Cube.java
@@ -0,0 +1,31 @@
+public class Cube {
+    private int side;
+    public Cube() {
+        this.side = 1;
+    }
+    public Cube(int side) {
+        checkSide(side);
+        this.side = side;
+    }
+    public int getSide() {
+        return side;
+    }
+    public void setSide(int side) {
+        checkSide(side);
+        this.side = side;
+    }
+    public int calculateSurfaceArea() {
+        return 6 * side * side;
+    }
+    public int calculateVolume() {
+        return side * side * side;
+    }
+    public String toString() {
+        return "Cube{side=" + side + "}";
+    }
+    public void checkSide(int side) {
+        if (side < 1) {
+            throw new IllegalArgumentException("A cube’s side length must be equal to or greater than 1!");
+        }
+    }  
+}
--- src/Cube2.java
+++ src/Cube2.java
@@ -1,90 +1,75 @@
 public class Cube2 {
     private Cube basicCube;
     private String color;
-
+    private int side;
+    public void checkSide(int side) {
+        if (side < 1) {
+            throw new IllegalArgumentException("A cube's side length must be equal to or greater than 1!");
+        }
+    }  
     public Cube2() {
-        basicCube = new Cube();
+        this.basicCube = new Cube();
+        this.side = 1;
         this.color = "black";
     }
-
     public Cube2(int side) {
-        this();
-        basicCube.setSide(side);
+        checkSide(side);
+        this.basicCube = new Cube(side);
+        this.side = side;
+        this.color = "black";
     }
-
-    public void setSide(int side) {
-        if (side < 1) {
-            throw new IllegalArgumentException("A cube’s side length must be equal to or greater than 1!");
-        }
-        basicCube.setSide(side);
+    public Cube2(int side, String color) {
+        checkSide(side);
+        this.basicCube = new Cube(side);
+        this.side = side;
+        this.color = color;
     }
-
     public int getSide() {
-        return basicCube.getSide();
+        return side;
     }
-
+    public void setSide(int side) {
+        checkSide(side);
+        this.side = side;
+        this.basicCube.setSide(side);
+    }
     public String getColor() {
         return color;
     }
-
     public void setColor(String color) {
         this.color = color;
-    }
-
-
-    //the following methods have been started for you, but need to be completed
-    public int calculateVolume() {
-        return basicCube.calculateVolume();
     }
     public int calculateSurfaceArea() {
         return basicCube.calculateSurfaceArea();
     }
-    public Cube2 add(Cube2 otherCube) {
-        if (Math.sqrt(Cube2.this.getSide() * Cube2.this.getSide() + otherCube.getSide() * otherCube.getSide())%1==0) {
-            int newSide = (int) Math.sqrt(Cube2.this.getSide() * Cube2.this.getSide() + otherCube.getSide() * otherCube.getSide());
-            if (newSide < 1) {
-                throw new IllegalArgumentException("A cube’s side length must be equal to or greater than 1!");
-            }  
-            return new Cube2(newSide);
+    public int calculateVolume() {
+        return basicCube.calculateVolume();
+    }
+    public String toString() {
+        return "Cube{side=" + Cube2.this.side + ", color=\"" + Cube2.this.color + "\"}";
+    }
+    public Cube2 add(Cube2 otherOne) {
+        if (Math.sqrt(Cube2.this.side * Cube2.this.side + otherOne.side * otherOne.side)%1 == 0) {
+        int newSide = (int) Math.sqrt(Cube2.this.side * Cube2.this.side + otherOne.side * otherOne.side);
+        checkSide(newSide);
+        return new Cube2(newSide, Cube2.this.color);
         } else {
-            throw new IllegalArgumentException("Resulting side length is invalid!");
+            throw new IllegalArgumentException("The cubes' side will not be an integer!");
         }
     }
-    public Cube2 minus(Cube2 otherCube) {
-        if (Cube2.this.getSide() >= otherCube.getSide()) {
-            if (Math.sqrt(Cube2.this.getSide() * Cube2.this.getSide() - otherCube.getSide() * otherCube.getSide())%1==0) {
-                int newSide = (int) Math.sqrt(Cube2.this.getSide() * Cube2.this.getSide() - otherCube.getSide() * otherCube.getSide());
-                if (newSide < 1) {
-                    throw new IllegalArgumentException("A cube’s side length must be equal to or greater than 1!");
-                }
-                return new Cube2(newSide);
-            } else {
-                throw new IllegalArgumentException("Resulting side length is invalid!");
-            }
-        } else if (Cube2.this.getSide() < otherCube.getSide()) {
-            if (Math.sqrt(otherCube.getSide() * otherCube.getSide() - Cube2.this.getSide() * Cube2.this.getSide())%1==0) {
-                int newSide = (int) Math.sqrt(otherCube.getSide() * otherCube.getSide() - Cube2.this.getSide() * Cube2.this.getSide());
-                if (newSide < 1) {
-                    throw new IllegalArgumentException("A cube’s side length must be equal to or greater than 1!");
-                }
-                return new Cube2(newSide);
-            } else {
-                throw new IllegalArgumentException("Resulting side length is invalid!");
-            }
+    public Cube2 minus(Cube2 otherOne) {
+        if (Math.sqrt(Cube2.this.side * Cube2.this.side - otherOne.side * otherOne.side)%1 == 0) {
+            int newSide = (int) Math.sqrt(Math.sqrt((Cube2.this.side * Cube2.this.side - otherOne.side * otherOne.side)*(Cube2.this.side * Cube2.this.side - otherOne.side * otherOne.side)));
+            checkSide(newSide);
+            return new Cube2(newSide, Cube2.this.color);
         } else {
-            throw new IllegalArgumentException("Resulting side length is invalid!");
+            throw new IllegalArgumentException("The cubes' side will not be an integer!");
         }
-        
     }
-    public boolean equals(Cube2 otherCube) {
-        if (this.getSide() == otherCube.getSide() && this.getColor().equals(otherCube.getColor())) {
+    public boolean equals(Cube2 otherOne) {
+        if ((Cube2.this.side == otherOne.side) & (Cube2.this.color.equals(otherOne.color))) {
             return true;
         } else {
             return false;
         }
-    }
-
-    public String toString() {
-        return "Cube{side=" + getSide() + ", color=" + color + "}";
     }
 }
